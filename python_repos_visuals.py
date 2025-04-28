c8da3d5da53170c23b54b21da4d78ae4f95132f5
tjhootman <tjhootman@gmail.com>
2025-04-28 19:14:52 -0400

    Process full API request and create bar chart using plotly. Update requirements.txt

--- python_repos_visuals.py
+++ python_repos_visuals.py
@@ -1,4 +1,5 @@
 import requests
+import plotly.express as px
 
 # Make an API call and check the response.
 url = "https://api.github.com/search/repositories"
@@ -8,20 +9,17 @@ headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 
-# Convert the response object to a dictionary.
+# Process overall results
 response_dict = r.json()
-print(f"Total repositories: {response_dict['total_count']}")
 print(f"Complete results: {not response_dict['incomplete_results']}")
 
-# Explore information about the repositories.
+# Process repository information.
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
-
-print("\nSelected information about each repository:")
+repo_names, stars = [], []
 for repo_dict in repo_dicts:
-    print(f"Name: {repo_dict['name']}")
-    print(f"Owner: {repo_dict['owner']['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f"Description: {repo_dict['description']}")
-    print()
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Create visualization.
+fig = px.bar(x=repo_names, y=stars)
+fig.show()
