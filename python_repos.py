bf694f4d7eec393e1219261e3031ac2ebcc696ba
tjhootman <tjhootman@gmail.com>
2025-04-28 18:50:00 -0400

    Return initial data of python repositories above 10,000 stars

--- python_repos.py
+++ python_repos.py
@@ -10,6 +10,15 @@ print(f"Status code: {r.status_code}")
 
 # Convert the response object to a dictionary.
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
 
-# Process results.
-print(response_dict.keys())
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
