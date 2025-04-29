1ec8981276687969602309567f5e60dc845e4a9b
tjhootman <tjhootman@gmail.com>
2025-04-28 21:26:01 -0400

    Customize marker colors

--- python_repos_visuals.py
+++ python_repos_visuals.py
@@ -40,4 +40,6 @@ fig = px.bar(x=repo_links, y=stars, title=title, labels=labels,
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
                   yaxis_title_font_size=20)
 
+fig.update_traces(marker_color='SteelBlue', marker_opacity=0.6)
+
 fig.show()
