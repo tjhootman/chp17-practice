db3eeac17f08c78e95698f2becd3e938f091edc1
tjhootman <tjhootman@gmail.com>
2025-04-28 21:13:51 -0400

    Add hover texts

--- python_repos_visuals.py
+++ python_repos_visuals.py
@@ -15,15 +15,22 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 
 # Process repository information.
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_names, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
 
+    # Build hover texts.
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
+
 # Create visualization.
 title = "Most-Starred Python Projects on GitHub"
 labels = {'x': 'Repository', 'y': 'Stars'}
-fig = px.bar(x=repo_names, y=stars, title=title, labels=labels)
+fig = px.bar(x=repo_names, y=stars, title=title, labels=labels,
+             hover_name=hover_texts)
 
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
                   yaxis_title_font_size=20)
